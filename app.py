ae3dcd300e115d72a23b4ee6183e5dcd735b1982
thepthut <67030296@kmitl.ac.th>
2024-10-14 14:53:54 +0700

    หน้าเว็บเบื้องต้น
    ปรับให้สามารถใช้งานได้

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ app = Flask(__name__, template_folder=template_dir)
 def index():
        return render_template('index.html')
 
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+@app.route('/total_summary')
+def total_summary():
+    return render_template('Total_summary.html')
+
 if __name__ == '__main__':
        print("Template folder:", template_dir)
        app.run(debug=True)
